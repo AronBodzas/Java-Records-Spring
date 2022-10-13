2cfb534956b6e6a943990764e129fd6cd49fc5b7
AronBodzas <aron.bodzas42@gmail.com>
2022-10-13 10:17:23 +0200

    Feat: more fields for user entity

--- backend/src/main/java/com/codecool/model/RegisteredUser.java
+++ backend/src/main/java/com/codecool/model/RegisteredUser.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,5 +14,12 @@ public class RegisteredUser {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
 
 }
