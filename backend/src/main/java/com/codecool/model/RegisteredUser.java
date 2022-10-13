5e8c4b1364594948c78598e69e9b2e4aa3cec243
AronBodzas <aron.bodzas42@gmail.com>
2022-10-13 10:01:55 +0200

    Feat: create entity for user

---
+++ backend/src/main/java/com/codecool/model/RegisteredUser.java
@@ -0,0 +1,20 @@
+package com.codecool.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegisteredUser {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
