14ea697f9291125940a7de97b049eb47c0a4a43f
AronBodzas <aron.bodzas42@gmail.com>
2022-10-13 10:01:02 +0200

    Feat: create repositories for entities

---
+++ backend/src/main/java/com/codecool/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.repository;
+
+import com.codecool.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
