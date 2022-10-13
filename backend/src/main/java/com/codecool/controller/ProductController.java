965542ed1cd498911a019cfa667897e8d493c6c3
AronBodzas <aron.bodzas42@gmail.com>
2022-10-13 10:03:05 +0200

    Feat: create controller classes

---
+++ backend/src/main/java/com/codecool/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.codecool.controller;
+
+import com.codecool.model.Product;
+import com.codecool.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class ProductController {
+
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("allProduct")
+    public List<Product> getAllProducts(){
+        return null;
+    }
+
+}
